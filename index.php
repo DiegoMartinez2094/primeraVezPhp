67bb1bf158997f563e623fd65254a07c1200dc2b
DiegoMartinez2094 <diegofernandomartinezcalderon@gmail.com>
2023-05-29 17:10:58 -0500

    feat: :construction_worker: Variables y Constatntes

--- index.php
+++ index.php
@@ -7,4 +7,22 @@ $texto = "mundo";
 printf("hola %s",$texto);/**solo imprime una cadena de texto a la vez. %s indica que debe tomar ese lugar la variable */
 
 
+
+
+/**variables y constantes */
+  
+$texto = "mundo"; /**variable tipo string*/
+$edad = 25; /**variable de tipo numerica */
+$es_valido= true; /**variable de tipo booleana */
+
+/**declarar una constante numerica */
+define("PI", 3.1416);
+
+/**declarar una constante booleana */
+define("ES_VALIDO",true);
+
+/**declarar una constante tipo texto */
+define("ES_TEXTO", "hola mundo");
+
+
 ?>

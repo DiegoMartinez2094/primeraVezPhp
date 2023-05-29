8c9ad54c2c4eee89cf7072b2b8b3f3359a6fe741
DiegoMartinez2094 <diegofernandomartinezcalderon@gmail.com>
2023-05-29 17:01:28 -0500

    feat: :construction_worker: Funciones de salida

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+/**funciones de salidas */
+
+echo '<h1> Hola mundo</h1>'; /** para imprimir una o varias cadenas  */
+
+$texto = "mundo";
+printf("hola %s",$texto);/**solo imprime una cadena de texto a la vez. %s indica que debe tomar ese lugar la variable */
+
 
 ?>

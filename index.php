76a00f8ae99f7b9925bb18f8de89274e2b662d93
DiegoMartinez2094 <diegofernandomartinezcalderon@gmail.com>
2023-05-30 17:31:09 -0500

    feat: :construction: ARRAYS

--- index.php
+++ index.php
@@ -46,5 +46,72 @@ $division=$numero1/$numero2; /**se utiliza el / */
 $modulo=$numero1%$numero2; /**se utiliza el % es el reciduo de la division*/
 $exponenciacion=$numero1**$numero2; /**se utiliza el ** para elevar un numero a una potencia de otro */
 
+/**Asignación */
+$asignacion = 5; /** se utiliza el = para signar un valor a una variable o constante */
+
+/** OPERADOR */
+if($numero1==0 & $numero2==0) /** operador "Y" */ 
+if($numero1==0 | $numero2==0) /** operador "O" */
+if($numero1==0 >> $numero2==0) /** operador SHIFT RIGHT */
+if($numero1==0 << $numero2==0) /** operador SHIFT LEFT */
+if($numero1==0 > $numero2==0) /** operador mayor que  */
+if($numero1==0 < $numero2==0) /** operador menor que  */
+if($numero1==0 == $numero2==0) /** operador comparacion */
+
+/** -1 si izq es menor
+ * 0 si es igual
+ * 1 si izq en mayor
+  */
+var_dump($numero1<=>$numero2);
+
+$texto3 = "Hola mundo";
+
+echo strlen($texto3); /** elimina los espacios */
+echo strtoupper($numero3); /** convertir a mayusculas*/
+echo strtolower($numero3); /**convertir a minusculas */
+echo strpos($texto3,'Hola'); /**verifica si un string existe o no */
+
+
+/** ARRAYS */
+
+$pc = ['pantalla', 'mouse', 'cpu'];
+echo $pc[1];
+
+/**Array asociativo */
+
+$pc1=array(
+    "pantalla"=>"samsung",
+    "mouse"=>"sony",
+    "teclado"=> "logitech"
+);
+
+echo $pc1["mouse"]; //**imprime sony*/
+
+
+/**foreach= para recorrer un array en este clase vamos a imprimirlo con su clave y valor */
+foreach($pc as $clave => $valor){
+    echo "clave: " . $clave . ",valor: " . $valor;
+}
+
+/**buscar elementos en un array */
+
+var_dump(in_array("pantalla",$pc));
+
+/**ordenar elementos de una array */
+
+$paraOrdenar = array(1, 2, 3, 4, 5);
+sort($paraOrdenar); /** de menor a mayor */
+rsort($paraOrdenar);/**de mayor a menor */
+
+/**ordenar arreglo asociativo */
+
+
+
+
+  
+
+
+
+
 
 ?>

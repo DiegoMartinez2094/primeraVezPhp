f037d9f503581c7767b95ce112ae1ee974bde5e7
DiegoMartinez2094 <diegofernandomartinezcalderon@gmail.com>
2023-06-01 17:13:04 -0500

    feat: :construction_worker: Avance Metodos Arrays

--- index.php
+++ index.php
@@ -103,15 +103,66 @@ $paraOrdenar = array(1, 2, 3, 4, 5);
 sort($paraOrdenar); /** de menor a mayor */
 rsort($paraOrdenar);/**de mayor a menor */
 
-/**ordenar arreglo asociativo */
+/**METODOS ARRAYS */
+
+/**metodo  array_flip(); intercambia llaves por el contenido de cada una*/
+
+$nombres = array("Diego" => 1, "Fernando" => 2, "Carlos" => 3);
+$flipped = array_flip($nombres);
+print_r($flipped); //Array ( [1] => Diego [2] => Fernando [3] => Carlos )
+
+/**metodo array_fill(): rellena un array este necesita 3 parametros 1=desde que posicion del array quiere que empiece a rellenar
+ * 2=cuantos espacios quiere que rellene y el 3=elcontenido con el que va a rellenar
+ */
+
+$fruta = array_fill(5, 6, 'pera');
+print_r($fruta);/** Matriz [5] => pera, [6] => pera,  [7] => pera, [8] => pera,  [9] => pera, [10] => pera
+*/ 
+
+/* array_filter(): Filtra los elementos de un array utilizando una función de devolución de llamada. *////////////////////////////////////////////////////////////
 
 
+/*array_map(): se define una funcion que va a ser aplicable a cada elemento del array y devolverá un nuevo array con el resultado*/
+function cuadrado($n)
+{
+ return ($n * $n);
+}
 
+$a = [1, 2, 3, 4,];
+$b = array_map('cuadrado', $a);
+print_r($b);/**$b=[1, 4, 9, 16] */
 
-  
+/**array_reduce(): define una funcion para aplicarle a un conjunto de elementos uno tras otro */
 
+function suma($acumulador, $num) /**DEFINIMOS la funcion */
+{
+ $acumulador += $num;
+ return $acumulador;
+}
 
+$conjunto= array(1,2,3,4);
 
+$result_sum = array_reduce($conjunto,'suma');
+print_r($result_sum);/**result_sum=10*/
+
+/**array_key_exists(): se utiliza solo en arrays asociativos y comprueba si exite un key en especifico retorna TRUE o FALSE */
+$nombres2 = array("Diego" => 1, "Fernando" => 2, "Carlos" => 3);
+$existKey= array_key_exists("Diego",$nombres2); /** se ingresaprimero la key a buscar seguido del array donde la va a buscar */
+print_r($existKey); /**TRUE*/
+
+/**in_array(): comprueba que exista un valor esprecifico en el array y devuelve TRUE o FALSE */
+
+$cities=["Bucaramanga", "Giron", "piedecuesta"];
+if (in_array("bucaramanga",$city)){ /**se ingres a primero el elemento a buscar, seguido del array donde lo buscaremos, esta funcion retorna un TRUE o FALSE */
+    echo( "si exite la ciudad Bucaramanga en el array");
+}
+
+/**array_rand(): Devuelve una o varias claves aleatorias de un array. */
+$nombres4 = array("Diego" => 1, "Fernando" => 2, "Carlos" => 3);
+$randed = array_rand($nombres4);
+print_r($randed); /**devue: "Diego" */
+
+/**array_unique(): Elimina los valores duplicados de un array. */
 
 
 ?>
